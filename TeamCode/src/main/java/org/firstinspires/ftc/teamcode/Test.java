88bca184217f2b4b3cdea6e301e44a48639b0dee
D <32046476+aWZHY0yQH81uOYvH@users.noreply.github.com>
2017-09-22 17:43:05 -0700

    does this work?

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+
+@TeleOp(name="Test", group="AlBot")
+@Disabled
+
+public class Test extends OpMode {
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void loop() {
+    }
+}
