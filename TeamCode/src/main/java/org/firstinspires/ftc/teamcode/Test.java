db188372f99f9d207ad6a6e6274b7bb493c64fb8
D <32046476+aWZHY0yQH81uOYvH@users.noreply.github.com>
2017-09-23 20:45:22 -0700

    println doesn't work here

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -3,23 +3,25 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.*;
 
 @TeleOp
+@Disabled
 public class Test extends OpMode {
     @Override
     public void init() {
         telemetry.addData("Status", "potato");
-        System.out.println("init");
     }
 
     @Override
     public void init_loop() {
+
     }
 
     @Override
     public void start() {
-        System.out.println("start");
+
     }
 
     @Override
     public void loop() {
+
     }
 }
