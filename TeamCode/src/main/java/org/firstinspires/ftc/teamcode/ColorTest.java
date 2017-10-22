88cf945193542278156e5b8f6721a6d32965ad7c
D <32046476+aWZHY0yQH81uOYvH@users.noreply.github.com>
2017-10-22 13:50:18 -0700

    hopefully added color test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorTest.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
@@ -14,14 +15,14 @@ import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 @TeleOp
 public class ColorTest extends LinearOpMode {
 
-    NormalizedColorSensor colorSensor;
-
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry.addData("Status", "Initializing");
         telemetry.update();
 
-
+        ColorSensor color = null;
+        color = hardwareMap.colorSensor.get("color");
+        color.enableLed(false);
 
         telemetry.addData("Stats", "Initialized");
         telemetry.update();
@@ -29,6 +30,12 @@ public class ColorTest extends LinearOpMode {
         waitForStart();
 
         while(opModeIsActive()) {
+
+            String probableColor = "";
+            if(color.blue()>=2&&color.red()<2&&color.green()<2) probableColor="blue";
+            else if(color.red()>=2&&color.blue()<2&&color.green()<2) probableColor="red";
+            else probableColor="?";
+            telemetry.addData("Color", "Probably "+probableColor);
 
             telemetry.update();
         }
