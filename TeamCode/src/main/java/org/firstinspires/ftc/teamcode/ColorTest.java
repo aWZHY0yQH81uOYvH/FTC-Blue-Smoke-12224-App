c4614056f1968e7cfca9e236f113dede8459f730
D <32046476+aWZHY0yQH81uOYvH@users.noreply.github.com>
2017-10-22 13:10:15 -0700

    added color sensor test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorTest.java
@@ -3,13 +3,19 @@ package org.firstinspires.ftc.teamcode;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
+import android.graphics.Color;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
 
 @TeleOp
 public class ColorTest extends LinearOpMode {
+
+    NormalizedColorSensor colorSensor;
+
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry.addData("Status", "Initializing");
